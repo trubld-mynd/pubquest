79980a6b1d9a992c7838290515d0ddd276c7f34a
Michael J Dowling <trubld.mynd@gmail.com>
2014-09-06 16:03:30 +1000

    Set up map on staticpage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require turbolinks
 //= require underscore
 //= require gmaps/google
-//= require turbolinks
 //= require_tree .
